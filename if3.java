944493b8dc1b739d2ce9dde96cc6c4652330edde
rohfarim <91410691+rohfarim@users.noreply.github.com>
2021-11-20 11:02:57 +0900

    Add files via upload

---
+++ if3.java
@@ -0,0 +1,19 @@
+package JAVA;
+
+import java.util.Scanner;
+
+public class if3 {
+
+    public static void main(String[] args) {
+        Scanner in = new Scanner(System.in);
+
+        int a = in.nextInt();
+
+        if(a%4==0) {
+            if(a%400==0) System.out.println("1");
+            else if(a%100==0) System.out.println("0");
+            else System.out.println("1");
+        }
+        else System.out.println("0");
+    }
+}
