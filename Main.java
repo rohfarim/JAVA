1ce90b7058df3f4ef3250d49cf71aa92006bd5af
rohfarim <91410691+rohfarim@users.noreply.github.com>
2021-12-08 18:20:06 +0900

    배열 3번째 문제

--- Main.java
+++ Main.java
@@ -2,22 +2,22 @@ package JAVA;
 import java.util.Scanner;
 public class Main {
     public static void main(String[] args) {
-        Scanner sc = new Scanner(System.in);
-        int arr [] = new int[9];
-        int max = 0;
-        int max_length = 0;
+      Scanner sc = new Scanner(System.in);
 
-        for (int i = 0; i< arr.length; i++ ){
-            arr[i] = sc.nextInt();
-        }
+      int a = sc.nextInt();
+      int b = sc.nextInt();
+      int c = sc.nextInt();
 
-        for (int i = 0; i< arr.length; i++ ) {
-           if (arr[i] > max){
-               max = arr [i];
-               max_length = i;
-           }
-        }
-        System.out.println(max);
-        System.out.println(max_length + 1);
+      int [] arr= new int[10];
+      int multi = a*b*c;
+
+      while (multi>0){
+          arr[multi %10]++;
+
+          multi /= 10;
+      }
+      for(int i=0; i< arr.length; i++){
+          System.out.println(arr[i]);
+      }
 }
 }
